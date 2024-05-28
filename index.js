8d27d3bb540b1b667d740f4d5d644f84bebb694e
s111ew <167631312+s111ew@users.noreply.github.com>
2024-05-28 19:21:47 +0100

    Add logic to get computer choice and get player choice through console input

--- index.js
+++ index.js
@@ -9,3 +9,29 @@
 // LOOP UNTIL 3 GAMES WON BY PLAYER OR COMPUTER
 // PRINT WINNER
 
+let playerScore = 0;
+let computerScore = 0;
+const prompt = require("prompt-sync")();
+
+let computerChoice;
+let playerChoice;
+
+const getComputerChoice = () => {
+    computerChoice = Math.random();
+    if (computerChoice <= 0.3333) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 0.6666) {
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors"
+    }
+}
+
+const getPlayerChoice = () => {
+    playerChoice = prompt("Choose Rock, Paper or Scissors: ").toLowerCase();
+    if ((playerChoice != "rock") && (playerChoice != "paper") && (playerChoice != "scissors")) {
+        console.log(`${playerChoice} is not a valid choice, please try again..`);
+        getPlayerChoice();
+    }
+}
+
